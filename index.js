beaaeb083e016238799ee2b1cbfefb4ee1361a07
Jonathan Jonsson <jonathancjonsson@outlook.com>
2022-02-18 14:45:22 +0100

    Create function operate
    Operate takes three parameters, the first choses the operator and the two other choses the numbers to calculate

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let add = (num, num2) => {
+function add(num, num2) {
   return num + num2;
-};
+}
 
 let subtract = (num, num2) => {
   return num - num2;
@@ -12,4 +12,18 @@ let multiply = (num, num2) => {
 
 let divide = (num, num2) => {
   return num / num2;
+};
+
+let operate = (operator, num, num2) => {
+  // calls one of the operators
+  switch (operator) {
+    case "add":
+      return add(num, num2);
+    case "subtract":
+      return subtract(num, num2);
+    case "multiply":
+      return multiply(num, num2);
+    case "divide":
+      return divide(num, num2);
+  }
 };
